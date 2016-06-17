b93acd8e7beb1dc3d767dd94a0c846a22149025d
Mike Dixon <mike@computerminds.co.uk>
2016-06-17 06:54:38 +0100

    Change how the map works to allow for no lat/long to be added if needed

--- js/geofield_gmap_latlong.js
+++ js/geofield_gmap_latlong.js
@@ -4,30 +4,25 @@
   var mapMarker = {};
   var defaultCoords = {
     'lat' : 51.507,
-    'long' : -0.129
+    'lng' : -0.129
   };
 
   var initMap = function(coords) {
     if ($.isEmptyObject(coords)) {
       coords.lat = defaultCoords.lat;
-      coords.long = defaultCoords.long;
+      coords.lng = defaultCoords.lng;
     }
 
     return new google.maps.Map(document.getElementById('googlemaps_canvas'), {
       zoom: 9,
-      center: new google.maps.LatLng(coords.lat, coords.long),
+      center: new google.maps.LatLng(coords.lat, coords.lng),
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
   };
 
-  var initMarker = function(coords) {
-    if ($.isEmptyObject(coords)) {
-      markerCoords.lat = defaultCoords.lat;
-      markerCoords.long = defaultCoords.long;
-    }
-
+  var initMarker = function() {
     return new google.maps.Marker({
-      position: new google.maps.LatLng(markerCoords.lat, markerCoords.long),
+      position: new google.maps.LatLng(markerCoords.lat, markerCoords.lng),
       draggable: true
     });
   };
@@ -35,11 +30,11 @@
   var addGmapDragListener = function(marker, context) {
     google.maps.event.addListener(marker, 'dragend', function (evt) {
       markerCoords.lat = evt.latLng.lat().toFixed(3);
-      markerCoords.long = evt.latLng.lng().toFixed(3);
+      markerCoords.lng = evt.latLng.lng().toFixed(3);
 
-      // Update the field lat / long from the new coords
+      // Update the field lat / lng from the new coords
       $(context).find('.field--type-geofield .geofield-lat').val(markerCoords.lat);
-      $(context).find('.field--type-geofield .geofield-lon').val(markerCoords.long);
+      $(context).find('.field--type-geofield .geofield-lon').val(markerCoords.lng);
     });
   };
 
@@ -48,33 +43,33 @@
       $(context).find('.field--type-geofield .geofield-lon').val()) {
 
       markerCoords.lat = $(context).find('.field--type-geofield .geofield-lat').val();
-      markerCoords.long = $(context).find('.field--type-geofield .geofield-lon').val();
+      markerCoords.lng = $(context).find('.field--type-geofield .geofield-lon').val();
     }
   };
 
   var updateMarkerCoordsFromFieldValues = function(context) {
-    // Update the field lat / long from the new coords
+    // Update the field lat / lng from the new coords
     $('.field--type-geofield .geofield-lat, .field--type-geofield .geofield-lon').change(function() {
       if ($(context).find('.field--type-geofield .geofield-lat').val() &&
         $(context).find('.field--type-geofield .geofield-lon').val()) {
 
         markerCoords.lat = $(context).find('.field--type-geofield .geofield-lat').val();
-        markerCoords.long = $(context).find('.field--type-geofield .geofield-lon').val();
+        markerCoords.lng = $(context).find('.field--type-geofield .geofield-lon').val();
 
-        if ($.isNumeric(markerCoords.lat) && $.isNumeric(markerCoords.long)) {
-          newLatLong = new google.maps.LatLng(markerCoords.lat, markerCoords.long);
+        if ($.isNumeric(markerCoords.lat) && $.isNumeric(markerCoords.lng)) {
+          newLatLong = new google.maps.LatLng(markerCoords.lat, markerCoords.lng);
           mapMarker.setPosition(newLatLong);
           map.setCenter(mapMarker.position);
           mapMarker.setMap(map);
         }
         else {
-          alert('Invalid value entered for lat / long, please ensure the coordinate(s) are valid.');
+          alert('Invalid value entered for lat / lng, please ensure the coordinate(s) are valid.');
         }
       }
     });
   };
 
-  Drupal.behaviors.geofieldGmapLatlong = {
+  Drupal.behaviors.geofieldGmapLatlng = {
     attach: function (context, settings) {
 
       // Don't do anything if we're on field configuration
@@ -85,23 +80,34 @@
 
         // Initialize the map, marker
         map = initMap(markerCoords);
-        mapMarker = initMarker(markerCoords);
 
-        // Add the drag event listener
-        addGmapDragListener(mapMarker, context);
+        map.addListener('dblclick', function(e) {
 
-        // Setup the map, marker
-        map.setCenter(mapMarker.position);
-        mapMarker.setMap(map);
+          if (mapMarker.position === undefined) {
 
-        // Set the initial lat / long field value from the initial marker coords
-        if ($(context).find('.field--type-geofield .geofield-lat').val() === '' &&
-            $(context).find('.field--type-geofield .geofield-lon').val() === '') {
-          $(context).find('.field--type-geofield .geofield-lat').val(markerCoords.lat);
-          $(context).find('.field--type-geofield .geofield-lon').val(markerCoords.long);
-        }
+            markerCoords.lat = e.latLng.lat();
+            markerCoords.lng = e.latLng.lng();
 
-        updateMarkerCoordsFromFieldValues(context);
+            mapMarker = initMarker(e.latlng);
+
+            // Add the drag event listener
+            addGmapDragListener(mapMarker, context);
+
+            // Setup the map, marker
+            map.setCenter(mapMarker.position);
+            mapMarker.setMap(map);
+
+            // Set the initial lat / lng field value from the initial marker coords
+            if ($(context).find('.field--type-geofield .geofield-lat').val() === '' &&
+              $(context).find('.field--type-geofield .geofield-lon').val() === '') {
+              $(context).find('.field--type-geofield .geofield-lat').val(markerCoords.lat);
+              $(context).find('.field--type-geofield .geofield-lon').val(markerCoords.lng);
+            }
+
+            updateMarkerCoordsFromFieldValues(context);
+          }
+
+        });
       }
     }
   };
